56aa07677036de5a86abfeddc5b95a6bc7102ddc
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-11 20:15:14 +0300

    Loaded Fonts

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+
+  const [ isLoadedFonts ] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+
+  if (!isLoadedFonts) {
+    return <AppLoading />
+  }
+
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
