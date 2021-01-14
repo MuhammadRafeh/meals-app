ecf06e8721038315f3ae17fb0f602debec1e0380
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-14 11:13:29 +0300

    Adding Bottom Tab Navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,11 @@
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import colors from '../constants/colors';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 
 const MealsNavigator = createStackNavigator({
@@ -27,4 +29,9 @@ const MealsNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: MealsNavigator,
+    favorites: FavoritesScreen
+})
+
+export default createAppContainer(MealsFavTabNavigator);
