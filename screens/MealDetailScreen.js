17598d3e63ebbd37432756301f83063b4aba443e
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-01-15 08:48:04 +0300

    Added MealDetail Screen Content

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,51 +1,87 @@
 // italian, japanies
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { MEALS } from '../data/dummy-data';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import HeaderButton from '../components/HeaderButton';
+import React from "react";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
+import { ScrollView } from "react-native-gesture-handler";
+import DefaultText from "../components/DefaultText";
 
-const MealDetailScreen = props => {
-
-    const mealId = props.navigation.getParam('mealId');
-
-    const selectedMeal = MEALS.find(meal => meal.id === mealId)
-
+const ListItem = (props) => {
     return (
-        <View style={styles.screen}>
-            <Text>The Meal Detail Screen!</Text>
-            <Button title="Go to First Screen" onPress={() => {
-                props.navigation.popToTop();
-            }} />
+        <View style={styles.listItem}>
+            <DefaultText>{props.children}</DefaultText>
         </View>
     );
-}
+};
 
-MealDetailScreen.navigationOptions = navigationData => {
+const MealDetailScreen = (props) => {
+    const mealId = props.navigation.getParam("mealId");
 
-    const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+    return (
+        <ScrollView>
+            <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}m</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map((ingredient) => (
+                <ListItem>{ingredient}</ListItem>
+            ))}
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.steps.map((step) => (
+                <ListItem>{step}</ListItem>
+            ))}
+        </ScrollView>
+    );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+    const mealId = navigationData.navigation.getParam("mealId");
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
     return {
         headerTitle: selectedMeal.title,
-        headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item
-                title="Favorite"
-                iconName="ios-star"
-                onPress={() => {
-                    console.log('Mark as Favrt!')
-                }}
-            />
-        </HeaderButtons>
-    }
-}
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Favorite"
+                    iconName="ios-star"
+                    onPress={() => {
+                        console.log("Mark as Favrt!");
+                    }}
+                />
+            </HeaderButtons>
+        ),
+    };
+};
 
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+    image: {
+        width: "100%",
+        height: 200,
+    },
+    details: {
+        flexDirection: "row",
+        padding: 15,
+        justifyContent: "space-around",
+    },
+    title: {
+        fontFamily: "open-sans-bold",
+        fontSize: 22,
+        textAlign: "center",
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: "#ccc",
+        borderWidth: 1,
+        padding: 10,
+    },
 });
